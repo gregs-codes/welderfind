7bf5b918c18536d99964ef5c74015e5e82c7a849
Greg Shabanov <30425203+gregs-codes@users.noreply.github.com>
2025-04-26 11:45:58 -0700

    migrated DB to postgres

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,16 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+// backend/db.js
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const pool = new Pool({
+  host: 'dpg-d067ij1r0fns73fb56d0-a.oregon-postgres.render.com',
+  port: 5432,
+  user: 'admin5678',
+  password: 'DRBrRkGfKFS5CxgJ5Pti4spod8zHUGWi',
+  database: 'fixit5678',
+  ssl: {
+    rejectUnauthorized: false, // Important for Render.com's SSL
+  },
 });
-
 
 module.exports = pool;
