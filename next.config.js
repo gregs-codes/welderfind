7e1ac30c52813d765a5cac3f1692b8c012c0ee55
Greg Shabanov <30425203+gregs-codes@users.noreply.github.com>
2025-04-26 20:58:52 -0700

    Fix distDir configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 const isProd = process.env.NODE_ENV === "production";
 
-
-
 module.exports = {
     images: {
       unoptimized: true,
       domains: ["lh3.googleusercontent.com"], // Add your image domains here
     },
-    distDir: "build",
+    //distDir: "build",
     assetPrefix: isProd ? "/welderfind/" : "",
     basePath: isProd ? "/welderfind" : "",
     trailingSlash: true,
@@ -16,7 +14,7 @@ module.exports = {
       return [
         {
           source: "/api/:path*", // Proxy all requests starting with /api
-          destination: "http://localhost:5000/api/:path*", // Redirect to backend
+          destination: "https://welderfind-api.onrender.com/api/:path*", // Redirect to backend
         },
       ];
     },
