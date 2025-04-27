b713ae99b5ac20fd1f78e4b4c2a22ceaccd186ea
Greg Shabanov <30425203+gregs-codes@users.noreply.github.com>
2025-04-26 22:06:39 -0700

    Remove basePath for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-    images: {
-      unoptimized: true,
-      domains: ["lh3.googleusercontent.com"], // Add your image domains here
-    },
-    //distDir: "build",
-    assetPrefix: isProd ? "/welderfind/" : "",
-    basePath: isProd ? "/welderfind" : "",
-    trailingSlash: true,
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: "/api/:path*", // Proxy all requests starting with /api
-          destination: "https://welderfind-api.onrender.com/api/:path*", // Redirect to backend
-        },
-      ];
-    },
-  };
+  images: {
+    unoptimized: true,
+    domains: ["lh3.googleusercontent.com"], // Add your image domains here
+  },
+  trailingSlash: true,
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Proxy all requests starting with /api
+        destination: "https://welderfind-api.onrender.com/api/:path*", // Redirect to backend
+      },
+    ];
+  },
+};
--- src/app/components/WelderCard.js
+++ src/app/components/WelderCard.js
@@ -1,26 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function WelderCard({ welder }) {
   return (
     <div>
-        
-        <Link key={welder.id} href={`/welders/${welder.id}`}>
-            <div className="border rounded-lg shadow-md p-4 flex flex-col items-center cursor-pointer hover:shadow-lg transition-shadow">
-            <Image
-                src={welder.image}
-                alt={welder.name}
-                width={228} // Example width (adjust as needed)
-                height={228} // Example height (adjust as needed)
-                className="rounded-2xl"
-            />
-            <h2 className="text-xl text-gray-600 font-semibold mt-4">{welder.name}</h2>
-            <p className="text-gray-600">{welder.city}</p>
-            <p className="text-green-600 font-bold">{welder.hourlyRate}</p>
-            </div>
-        </Link>
+      <Link key={welder.id} href={`/welders/${welder.id}`}>
+        <div className="border rounded-lg shadow-md p-4 flex flex-col items-center cursor-pointer hover:shadow-lg transition-shadow">
+          <Image
+            src={welder.image} // Use the image path directly
+            alt={welder.name}
+            width={228} // Example width (adjust as needed)
+            height={228} // Example height (adjust as needed)
+            className="rounded-2xl"
+          />
+          <h2 className="text-xl text-gray-600 font-semibold mt-4">{welder.name}</h2>
+          <p className="text-gray-600">{welder.city}</p>
+          <p className="text-green-600 font-bold">{welder.hourlyRate}</p>
+        </div>
+      </Link>
     </div>
-
   );
 }
